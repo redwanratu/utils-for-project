d47f47575b5e2f169fb97e22b3e81d93c7511286
redwanratu <redwanulratu@gmail.com>
2022-09-23 14:13:43 +0600

    adding mvc file system  generator

---
+++ mvcfs.sh
@@ -0,0 +1,11 @@
+# !/bin/bash
+
+# Author: Redwan Ratu
+
+#Script  follows here
+
+mkdir models views controllers utils
+touch server.js app.js config.env 
+
+# prettierrc :  vs extension Prettier code formatter
+touch .prettierrc
